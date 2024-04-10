1a61f19bf08b96eadc4a018550e017c527df8388
openaixander <164517108+openaixander@users.noreply.github.com>
2024-04-10 10:37:37 +0100

    Update settings.py

--- dee_pastery/settings.py
+++ dee_pastery/settings.py
@@ -190,7 +190,7 @@ LOGIN_URL = 'accounts:login'
 
 SESSION_EXPIRE_SECONDS = 120  # 1 hour
 SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
-#SESSION_TIMEOUT_REDIRECT = 'accounts/login.html'
+SESSION_TIMEOUT_REDIRECT = '../accounts/templates/accounts/login.html'
 
 
 # EMAIL VERIFICATION
